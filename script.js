78e08543f02a0343ace53a13cbd0a574a1aaf433
Andrew Korshak <67147751+yakorshak@users.noreply.github.com>
2021-12-19 23:18:08 +0200

    updated JS

--- script.js
+++ script.js
@@ -4,45 +4,83 @@ const startButton = document.querySelector('.stopwatch__button-start');
 const pauseButton = document.querySelector('.stopwatch__button-pause');
 const resetButton = document.querySelector('.stopwatch__button-reset');
 const stopwatchDisplay = document.querySelector('.stopwatch__body');
-const startStopwatchValue = stopwatchDisplay.innerHTML.trim().split(':'); // = ['00', '00', '00', '00']
 
 
-startButton.addEventListener('click', function(event) {
+
+startButton.addEventListener('click', function start(event) {
+    
     if (event.target.closest('.stopwatch__button-start')){
-        console.log('click');
+        // startButton.removeEventListener('click', start); (что тут?)
+        let timerId = setTimeout(function tick(){
+            pauseButton.addEventListener('click', function pause(event){
+                event.target.closest('.stopwatch__button-pause');
+                clearTimeout(timerId);
+                return ;
+            });
+            resetButton.addEventListener('click', function reset(event){
+                event.target.closest('.stopwatch__button-reset');
+                clearTimeout(timerId);
+                resetStopwatch();
+                return;
+            });
+            updateStopwatch(); // функция при вызове которой будет апдейтиться дисплей
+            timerId = setTimeout(tick, 10);
+        },10);
     }
 });
 
-
-function updateStopwatch(array){
-    let stopwatchValue = array;
-    if (stopwatchValue[3] < 99){
-        stopwatchValue[3]++;
-    } else {
-        if (stopwatchValue[2] < 59){
-            stopwatchValue[3] = 0;
-            stopwatchValue[2]++;
-        } else {
-            if (stopwatchValue[1] < 59){
-                stopwatchValue[2] = 0;
-                stopwatchValue[1]++;
-            } else {
-                if (stopwatchValue[0] < 59){
-                    stopwatchValue[1] = 0;
-                    stopwatchValue[0]++;
-                } else {
-                    //stop
-                }
-            }    
-        }
-    }
-    let currentDisplay = document.querySelector('.stopwatch__body');
-    let stopwatchValueString = stopwatchValue.join(':')
-
-    currentDisplay.innerHTML = stopwatchValueString;
+function addZero(value){
+    let newValue = '0' + value;
+    return newValue;
 }
 
-let timerId = setTimeout(function tick(){
-    updateStopwatch(startStopwatchValue); // функция при вызове которой будет апдейтиться дисплей
-    timerId = setTimeout(tick, 10);
-},1000);
+function resetStopwatch(){
+    stopwatchDisplay.innerHTML = '00:00:00:00';
+}
+
+
+function updateStopwatch(){
+    let currentStopwatch = stopwatchDisplay.innerHTML.trim().split(':'); // = ['00', '00', '00', '00']
+    let [hours, minutes, seconds, miliseconds] = currentStopwatch;
+
+        if (miliseconds < 99){
+            if (miliseconds < 9){
+                miliseconds++;
+                miliseconds = addZero(miliseconds);
+            }
+            else {
+                miliseconds++;
+            }
+        } else if (seconds < 59){
+            if (seconds < 9){
+                seconds++;
+                seconds = addZero(seconds);
+            }
+            else {
+                seconds++;
+            }
+            miliseconds = '00';
+        } else if (minutes < 59){
+            if (minutes < 9){
+                minutes++;
+                minutes = addZero(minutes);
+            }
+            else {
+                minutes++;
+            }
+            seconds = '00';
+        } else if (hours < 59){
+            if (hours < 9){
+                hours++;
+                hours = addZero(hours);
+            }
+            else {
+                hours++;
+            }
+            minutes = '00';
+        } 
+    
+    let stopwatchValueString = (String(hours) + ':' + String(minutes) + ':' + String(seconds) + ':' + String(miliseconds));
+    stopwatchDisplay.innerHTML = stopwatchValueString;
+}
+
